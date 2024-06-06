bcf513e111a98f20ea31e3f07bb0a6158ec5d4a7
viniciussuares <v.suares.s@hotmail.com>
2024-06-06 05:19:04 -0300

    Refined query in data collection

--- src/config.py
+++ src/config.py
@@ -1,3 +1,5 @@
+import os 
+
 # Data Collection
 BILLING_PROJECT_ID = "incomeprediction-425511"
 SQL_QUERY = """
@@ -17,7 +19,19 @@ SELECT
     , V3001 AS alfabetizado
     , V3009 AS curso_mais_elevado
     , V3009A AS curso_mais_elevado_2
+    , V4076 AS tempo_desempregado_procurando_trabalho
+    , VD2002 AS condicao_domicilio
+    , VD2003 AS numero_componente_domicilio
+    , VD3005 AS anos_estudos
+    , VD4001 AS condicao_forca_trabalho
+    , VD4002 AS condicao_ocupacao
+    , VD4003 AS forca_trabalho_potencial
+    , VD4007 AS posicao_ocupacao_principal
+    , VD4016 AS rendimento_mensal_habitual_trabalho_principal
+    , VD4017 AS rendimento_mensal_efetivo_trabalho_principal
+    , VD4019 AS rendimento_mensal_habitual_todos_trabalhos
+    , VD4020 AS rendimento_mensal_habitual_todos_trabalhos
+    , VD4031 AS horas_semana_todos_trabalhos
 FROM basedosdados.br_ibge_pnadc.microdados
-LIMIT 10
 """
-
+COLLECTED_DATA_PATH = os.path.join(os.getcwd(), "..", "data", "collected_data.csv")
--- src/data_collection.py
+++ src/data_collection.py
@@ -4,5 +4,6 @@ import config
 
 # Initial test
 df = bd.read_sql(config.SQL_QUERY, billing_project_id=config.BILLING_PROJECT_ID)
+df.to_csv(config.COLLECTED_DATA_PATH)
 print(df.shape)
 
