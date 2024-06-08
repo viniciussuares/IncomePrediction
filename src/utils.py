65921c8600d9412f70290e270ef4356bdbd687aa
viniciussuares <v.suares.s@hotmail.com>
2024-06-08 04:32:49 -0300

    nulls_percentage in utils

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,10 @@
 """This file contains functions used for exploration, model selection, validation, etc"""
 import pandas as pd
 
+# EDA
 def read_compressed_data(data_path: str, compression='gzip'):
     return pd.read_csv(data_path, compression=compression)
+
+def nulls_percentage(df: pd.DataFrame):
+    print("Null Percentage by Column: ")
+    return df.isnull().mean().round(2) * 100
