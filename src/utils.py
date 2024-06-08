207a28054fbd437c6e3f80e65200c7622ae0492d
viniciussuares <v.suares.s@hotmail.com>
2024-06-08 07:06:13 -0300

    Finished steps 1 and 2 in EDA

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,7 @@
 """This file contains functions used for exploration, model selection, validation, etc"""
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 # EDA
 def read_compressed_data(data_path: str, compression='gzip'):
@@ -7,4 +9,23 @@ def read_compressed_data(data_path: str, compression='gzip'):
 
 def nulls_percentage(df: pd.DataFrame):
     print("Null Percentage by Column: ")
-    return df.isnull().mean().round(2) * 100
+    print("---"*20)
+    print(df.isnull().mean().round(2) * 100)
+
+def formatted_description(df: pd.DataFrame):
+    return df.describe().iloc[1:, :].round(1)
+
+def outlier_percentage(df: pd.DataFrame):
+    """Uses the fence method to identify the percentage of outliers per column"""
+    total_rows = df.shape[0]
+    result = {}
+    for col in df.columns:
+        q1 = df[col].quantile(.25)
+        q3 = df[col].quantile(.75)
+        iqr = q3 - q1
+        inner_fence = df.loc[(df[col] < q1 - 1.5*iqr) | (df[col] > q3 + 1.5*iqr)].shape[0]
+        outer_fence = df.loc[(df[col] < q1 - 3*iqr) | (df[col] > q3 + 3*iqr)].shape[0]
+        result[col] = [f'{inner_fence/total_rows * 100: .2f}%', f'{outer_fence/total_rows * 100: .2f}%']
+    
+    print('Percentage of Outliers per Column: ')
+    return pd.DataFrame(result, index=['Inner Fence', 'Outer Fence']).T
